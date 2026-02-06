27f162527d11998caa813810e19d158e911c1bd4
Nash Rojas <nashjuan02@gmail.comclearcls>
2026-02-06 19:19:56 -0400

    Creacion clase circulo

---
+++ Tarea Cap 6-7/Paquete_tarea_6_7/Circulo.java
@@ -0,0 +1,15 @@
+package Paquete_tarea_6_7;
+
+public class Circulo extends Figura {
+
+    private double radio;
+
+    public Circulo(double radio){
+        this.radio = radio;
+    }
+
+    @Override
+    public double area(){
+        return Math.PI * radio * radio;
+    }
+}
